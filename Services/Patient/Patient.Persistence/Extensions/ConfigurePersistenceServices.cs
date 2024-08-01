5e7372f1eba23a981eea5d7e2c55fa3a004dbf77
Md. Atikur Zaman <arzaman@ael-bd.com>
2024-08-01 14:49:30 +0600

    add configuration

---
+++ Services/Patient/Patient.Persistence/Extensions/ConfigurePersistenceServices.cs
@@ -0,0 +1,17 @@
+﻿namespace Patient.Persistence.Extensions;
+
+public static class ConfigurePersistenceServices
+{
+    public static WebApplicationBuilder AddPersistenceServices(this WebApplicationBuilder builder)
+    {
+        //builder.Services.AddDbContext<InventoryDbContext>(options =>
+        //{
+        //    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+        //});
+
+        //builder.Services.AddDbContext<InventoryDbContext>();
+        builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+        return builder;
+    }
+}
--- Services/Patient/Patient.Persistence/GlobalUsings.cs
+++ Services/Patient/Patient.Persistence/GlobalUsings.cs
@@ -1,1 +1,7 @@
-﻿global using Patient.Application.Contracts.Persistence;
+﻿global using Microsoft.AspNetCore.Builder;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.Extensions.Configuration;
+
+global using Patient.Persistence.Repositories;
+global using Patient.Application.Contracts.Persistence;
