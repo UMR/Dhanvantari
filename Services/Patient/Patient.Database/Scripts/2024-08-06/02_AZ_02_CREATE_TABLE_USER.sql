be35a2aa53eaba2afdc4f5432f2846b3c900ff92
Md. Atikur Zaman <arzaman@ael-bd.com>
2024-08-06 14:13:16 +0600

    add table script

---
+++ Services/Patient/Patient.Database/Scripts/2024-08-06/02_AZ_02_CREATE_TABLE_USER.sql
@@ -0,0 +1,34 @@
+﻿USE [DhanvantariDB]
+GO
+
+/****** Object:  Table [dbo].[User]    Script Date: 8/6/2024 2:10:25 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[User](
+	[Id] [uniqueidentifier] NOT NULL,
+	[FirstName] [nvarchar](50) NOT NULL,
+	[LastName] [nvarchar](50) NOT NULL,
+	[Mobile] [nvarchar](15) NULL,
+	[Email] [nvarchar](100) NULL,
+	[Password] [nvarchar](50) NOT NULL,
+	[DateOfBirth] [datetime] NULL,
+	[Status] [tinyint] NOT NULL,
+	[CreatedBy] [uniqueidentifier] NULL,
+	[CreatedDate] [datetime] NOT NULL,
+	[UpdatedBy] [uniqueidentifier] NULL,
+	[UpdatedDate] [datetime] NULL,
+ CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[User] ADD  CONSTRAINT [DF_User_Id]  DEFAULT (newid()) FOR [Id]
+GO
+
+
