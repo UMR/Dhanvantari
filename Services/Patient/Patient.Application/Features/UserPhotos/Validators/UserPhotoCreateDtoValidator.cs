9aa4b79a680ab6280f39ea20fde00456568e4f07
Md. Atikur Zaman <arzaman@ael-bd.com>
2024-08-09 16:21:26 +0600

    add and implement user photo validator

---
+++ Services/Patient/Patient.Application/Features/UserPhotos/Validators/UserPhotoCreateDtoValidator.cs
@@ -0,0 +1,27 @@
+﻿namespace Patient.Application.Features;
+
+public class UserPhotoCreateDtoValidator : AbstractValidator<UserPhotoCreateDto>
+{
+    private readonly IUserRepository _userRepository;
+
+    public UserPhotoCreateDtoValidator(IServiceProvider serviceProvider)
+    {
+        _userRepository = serviceProvider.GetService<IUserRepository>();
+
+        RuleFor(a => a.Photo)
+            .NotEmpty()
+            .WithMessage("{PropertyName} is required");
+
+        RuleFor(a => a.FileName)
+            .NotEmpty()
+            .WithMessage("{PropertyName} is required")
+            .MaximumLength(100)
+            .WithMessage("{PropertyName} must not exceed 100 characters");
+
+        RuleFor(a => a.UserId)           
+            .NotEmpty()
+            .WithMessage("{PropertyName} is required");       
+
+    }    
+    
+}
---
+++ Services/Patient/Patient.Application/Features/UserPhotos/Validators/UserPhotoUpdateDtoValidator.cs
@@ -0,0 +1,23 @@
+﻿namespace Patient.Application.Features;
+
+public class UserPhotoUpdateDtoValidator : AbstractValidator<UserPhotoUpdateDto>
+{
+    private readonly IUserRepository _userRepository;
+
+    public UserPhotoUpdateDtoValidator(IServiceProvider serviceProvider)
+    {
+        _userRepository = serviceProvider.GetService<IUserRepository>();
+
+        RuleFor(a => a.Photo)
+            .NotEmpty()
+            .WithMessage("{PropertyName} is required");
+
+        RuleFor(a => a.FileName)
+            .NotEmpty()
+            .WithMessage("{PropertyName} is required")
+            .MaximumLength(100)
+            .WithMessage("{PropertyName} must not exceed 100 characters");       
+
+    }
+    
+}
